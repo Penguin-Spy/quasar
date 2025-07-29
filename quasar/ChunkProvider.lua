58c8409af8db1ca8ae43755bb6c55aa2e7d7e8ad
Penguin_Spy <me@penguinspy.dev>
2025-07-28 23:36:37 -0700

    superflat chunk provider

--- quasar/ChunkProvider.lua
+++ quasar/ChunkProvider.lua
@@ -14,9 +14,10 @@
 
 local Chunk = require "quasar.Chunk"
 local Anvil = require "quasar.Anvil"
-local Registry = require("quasar.Registry")
+local Registry = require "quasar.Registry"
 
 local block_state_map = Registry.get_block_state_map()
+local AIR_BLOCKSTATE = block_state_map["minecraft:air"]
 local format, insert = string.format, table.insert
 
 local ChunkProvider = {}
@@ -79,36 +80,104 @@ function ChunkProvider.region(options)
 end
 
 
+local function superflat_provider_load(self, chunk_x, chunk_z)
+  return Chunk.new_from_data(self.sections)
+end
+
 ---@class (exact) ChunkProvider.superflat_options
 --- a list of block state and height pairs, in order from bottom to top of the chunk
 ---@field layers ([identifier, integer])[]
---- the biome for all chunks
----@field biome identifier
+--- (unused) the biome for all chunks
+----@field biome identifier
 --- the number of subchunks in this chunk; must match the height of the dimension's type
 ---@field subchunk_height integer
 --- unused
 ----@field size {x: integer, z: integer}
 
---- test
+--- A chunk provider that generates a superflat world.
 ---@param options ChunkProvider.superflat_options
 ---@return ChunkProvider
 function ChunkProvider.superflat(options)
-  error("superflat generator not implemented")
-  -- create subchunks from bottom up
-  --[=[local sections, current_height = {}, 0
-  for _, layer in pairs(options.layers) do
-    local state, height = block_state_map[layer[1]], layer[2]
-    -- local row =
-  end]=]
+  local sections, section_count = {}, 0
 
-  -- create list of block states for each individual layer
-  --[[local layers = {}
-  for _, layer in pairs(options.layers) do
-    for _ = 1, layer[2] do
-      table.insert(layers, layer[1])
+  local layer_index = 1
+  -- blockstate network id, long of 16 palette indexes,
+  local blockstate, blockstate_long, remaining_layer_height = nil, nil, 0
+
+  -- paletted block states long data array, map of block state id -> palette index
+  local block_states, palette_contents, palette_index = {}, {}, 0
+  local remaining_section_height = 16
+
+  while true do
+    local need_update_blockstate_palette = false
+
+    -- get next section if necessary
+    if remaining_section_height <= 0 then
+      local block_palette = {}
+      for blockstate_id, index in pairs(palette_contents) do
+        block_palette[index] = blockstate_id
+      end
+      insert(sections, {
+        block_states = block_states,
+        block_palette = block_palette
+      })
+      -- the only break condition; we've filled up all the sections we need to
+      if #sections == options.subchunk_height then break end
+
+      block_states, palette_contents, palette_index = {}, {}, 0
+      remaining_section_height = 16
+      need_update_blockstate_palette = true -- palette was cleared
     end
-  end]]
 
+    -- get next (or first) layer if necessary
+    if remaining_layer_height <= 0 then
+      local layer = options.layers[layer_index]
+      if layer then
+        layer_index = layer_index + 1
+        blockstate = block_state_map[layer[1]]
+        remaining_layer_height = layer[2]
+      else -- no more layers, fill with air
+        blockstate = AIR_BLOCKSTATE
+        remaining_layer_height = 16
+      end
+
+      need_update_blockstate_palette = true -- blockstate was changed
+    end
+
+    if need_update_blockstate_palette then
+      -- add blockstate into palette (if not there)
+      local blockstate_index = palette_contents[blockstate]
+      if not blockstate_index then
+        blockstate_index = palette_index
+        palette_contents[blockstate] = palette_index
+        palette_index = palette_index + 1
+        assert(blockstate_index <= 15, "cannot use more than 4 bits for a palette index!?")
+      end
+
+      -- calculate new long of palette index
+      blockstate_long = 0
+      for i = 0, 15*4, 4 do
+        blockstate_long = blockstate_long | (blockstate_index << i)
+      end
+    end
+
+    local height = math.min(remaining_section_height, remaining_layer_height)
+
+    -- insert longs into section blockstates
+    for _ = 1, height do  -- y layers
+      for _ = 1, 16 do    -- 16 z values
+        insert(block_states, blockstate_long) -- each long is 16 x values
+      end
+    end
+
+    remaining_layer_height = remaining_layer_height - height
+    remaining_section_height = remaining_section_height - height
+  end
+
+  return {
+    sections = sections,
+    load = superflat_provider_load
+  }
 end
 
 
